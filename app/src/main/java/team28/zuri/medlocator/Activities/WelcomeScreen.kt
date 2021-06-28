2e7833580ad41420cb7bc188a557fc2cb59f26ea
Ab3id <abeidtwo@gmail.com>
2021-06-28 22:36:27 +0300

    feat: add users home screen

--- app/src/main/java/team28/zuri/medlocator/Activities/WelcomeScreen.kt
+++ app/src/main/java/team28/zuri/medlocator/Activities/WelcomeScreen.kt
@@ -1,12 +1,20 @@
 package team28.zuri.medlocator.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import team28.zuri.medlocator.R
 
 class WelcomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_screen)
+        val btn_next:Button = findViewById(R.id.btn_welcome_next);
+
+        btn_next.setOnClickListener(){
+            startActivity(Intent(this,UserHomeScreen::class.java))
+        }
+
     }
 }
